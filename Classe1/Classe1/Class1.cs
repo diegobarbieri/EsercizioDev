7e2ac374efd5a04c2e790a9b097c0f62cfe52874
Diego <jdiegoba25@gmail.com>
2018-06-20 16:18:33 +0200

    Aggiunta la prima classe

---
+++ Classe1/Classe1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe1
+{
+    public class Class1
+    {
+        private string nomeClasse;
+        private string idClasse;
+    }
+}
